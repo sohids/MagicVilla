2752222f5e9054f0264733501b7c8b7b07e4030c
Sohid Ullah <Sohid.Ullah@ding.com>
2024-05-20 18:44:10 +0600

    added model

---
+++ src/MagicVilla.Api/Controllers/VillaController.cs
@@ -0,0 +1,21 @@
+﻿using MagicVilla.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla.Api.Controllers
+{
+    [Route("api/Blah")]
+    [ApiController]
+    public class VillaController: ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa { Id = 1, Name = "Pool View" },
+                new Villa { Id = 1, Name = "Beach View" },
+            };
+        }
+
+    }
+}
---
+++ src/MagicVilla.Api/Models/Villa.cs
@@ -0,0 +1,8 @@
+﻿namespace MagicVilla.Api.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
