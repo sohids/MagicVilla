b91848792b53be569040c179083bcc4e806edf46
Sohid Ullah <Sohid.Ullah@ding.com>
2024-06-08 12:39:56 +0600

    adding home page

--- src/MagicVilla.Web/Controllers/HomeController.cs
+++ src/MagicVilla.Web/Controllers/HomeController.cs
@@ -1,32 +1,32 @@
+using AutoMapper;
 using MagicVilla.Web.Models;
+using MagicVilla.Web.Services.IService;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
+using MagicVilla.Web.Models.Dto;
 
 namespace MagicVilla.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IVillaService _villaService;
+        private readonly IMapper _mapper;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IVillaService villaService, IMapper mapper)
         {
-            _logger = logger;
+            _villaService = villaService;
+            _mapper = mapper;
         }
-
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var list = new List<VillaDto>();
+            var response = await _villaService.GetVillaAsync<ApiResponse>();
+            if (response != null)
+            {
+                list = JsonConvert.DeserializeObject<List<VillaDto>>(response.Result.ToString() ?? string.Empty);
+            }
+            return View(list);
         }
     }
 }
